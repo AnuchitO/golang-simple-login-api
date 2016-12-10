db4db714b9219d3475257f63f96c3facdb98f590
Pead Nong <anuchit.prasertsang@gmail.com>
2016-12-10 22:35:10 +0700

    refactor to use http status constant

--- token/validator.go
+++ token/validator.go
@@ -1,13 +1,15 @@
 package token
 
+import "net/http"
+
 func CheckAuthorize(user, passwd string) (map[string]string, int) {
 	resp := map[string]string{}
 
 	if user == "kob@gmail.com" && passwd == "aobaob" {
 		resp["token"] = CreateToken(user)
-		return resp, 200
+		return resp, http.StatusOK
 	}
 
 	resp["error"] = "user or password wrong"
-	return resp, 401
+	return resp, http.StatusUnauthorized
 }
